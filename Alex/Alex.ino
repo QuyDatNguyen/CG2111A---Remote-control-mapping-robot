4d254f69c45a53b2afec110b27dae35a086ddc81
yuu <58155530+kagiura@users.noreply.github.com>
2024-04-12 10:53:40 +0800

    take out debug code

--- Alex/Alex.ino
+++ Alex/Alex.ino
@@ -262,7 +262,7 @@ void enablePullups()
 void leftISR()
 {
 
-  dbprintf("L ");
+  // dbprintf("L ");
   if (dir == FORWARD)
   {
     leftForwardTicks++;
@@ -286,7 +286,7 @@ void leftISR()
 
 void rightISR()
 {
-  dbprintf("R ");
+  // dbprintf("R ");
   if (dir == FORWARD)
   {
     rightForwardTicks++;
