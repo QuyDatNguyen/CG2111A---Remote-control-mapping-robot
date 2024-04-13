b5b95bcbf27e9c384cb74ef10205c66514fdfedd
yuu <58155530+kagiura@users.noreply.github.com>
2024-04-14 03:26:14 +0800

    debug ultrasonic

--- Alex/Alex.ino
+++ Alex/Alex.ino
@@ -463,10 +463,11 @@ int readUltrasonic()
 {
   delay(2);
   PORTL |= TRIG; // set HIGH
-  delay(10);
+  delay(15);
   PORTL &= ~TRIG; // set LOW
+  delay(60);
 
-  unsigned long duration = pulseIn(47, HIGH, TIMEOUT);   // read pulse
+  unsigned long duration = pulseIn(47, LOW, TIMEOUT);    // read pulse
   double dist = ((double)duration) / 2 * SPEED_OF_SOUND; // convert to mms
   // return dist;
   return (int)dist;
