3020e787f7e1dd2cd33b88167bcf44b56edd5798
yuu <58155530+kagiura@users.noreply.github.com>
2024-04-14 12:24:33 +0800

    switch to ncurses

--- alex-pi.cpp
+++ alex-pi.cpp
@@ -8,14 +8,7 @@
 #include "Alex/constants.h"
 #include "serial.h"
 #include "serialize.h"
-
-// for fun:
-#include <termios.h>
-#include <stdio.h>
-static struct termios old, current;
-void initTermios(int echo);
-void resetTermios(void);
-char getch_(int echo);
+#include <ncurses.h>
 
 // #define PORT_NAME "/dev/ttyACM1"
 #define PORT_NAME_WITHOUT_NUM "/dev/ttyACM"
@@ -373,7 +366,7 @@ int main(int argc, char *argv[])
         // scanf("%c", &ch);
         // above but without requiring an enter
         // something like getch() but cross platform
-        ch = getch_(0);
+        ch = getch();
 
         // Purge extraneous characters from input stream
         flushInput();
@@ -383,39 +376,4 @@ int main(int argc, char *argv[])
 
     printf("Closing connection to Arduino.\n");
     endSerial();
-}
-
-// fun stuff
-
-/* Initialize new terminal i/o settings */
-void initTermios(int echo)
-{
-    tcgetattr(0, &old);         /* grab old terminal i/o settings */
-    current = old;              /* make new settings same as old settings */
-    current.c_lflag &= ~ICANON; /* disable buffered i/o */
-    if (echo)
-    {
-        current.c_lflag |= ECHO; /* set echo mode */
-    }
-    else
-    {
-        current.c_lflag &= ~ECHO; /* set no echo mode */
-    }
-    tcsetattr(0, TCSANOW, &current); /* use these new terminal i/o settings now */
-}
-
-/* Restore old terminal i/o settings */
-void resetTermios(void)
-{
-    tcsetattr(0, TCSANOW, &old);
-}
-
-/* Read 1 character - echo defines echo mode */
-char getch_(int echo)
-{
-    char ch;
-    initTermios(echo);
-    ch = getchar();
-    resetTermios();
-    return ch;
 }
